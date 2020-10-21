3ba3fa836e0d5ebf9cc229726aa8e5c7383a778d
Cristian Novac <crysist14@gmail.com>
2020-10-21 14:23:43 -0400

    some more stuff

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,44 +1,54 @@
-const formEl = document.querySelector("form");
-const shortUrlEl = document.querySelector("#shortUrl");
+const formEl = document.querySelector('form');
+const shortUrlEl = document.querySelector('#shortUrl');
 
-formEl.addEventListener("submit", formSubmit);
+formEl.addEventListener('submit', formSubmit);
 
 //function ran when form is submitted
 function formSubmit(e) {
-  e.preventDefault(); //prevent page reload
+    e.preventDefault(); //prevent page reload
 
-  //grab values from input fields
-  var url = formEl.url.value;
-  var alias = formEl.alias.value;
-  console.log(url, alias);
+    //grab values from input fields
+    var url = DOMPurify.sanitize(formEl.url.value);
+    var alias = DOMPurify.sanitize(formEl.alias.value);
+    console.log(url, alias);
 
-  //send request to backend and receive the new url
-  const response = sendRequest(url, alias);
-  console.log("response", response);
+    //send request to backend and receive the new url
+    const response = sendRequest(url, alias);
+    console.log('response', response);
+    if (response && response.error) {
+        shortUrlEl.innerHTML = response.errors;
+    }
+    shortUrlEl.innerHTML = `<a href=${response.url}</a>`;
 
-  //insert url into span in the document
-  shortUrlEl.innerHTML = `<a href=${document.location.origin}/${alias}>${document.location.origin}/${alias}</a>`;
+    //insert url into span in the document
 
-  //clear input fields
-  formEl.url.value = "";
-  formEl.alias.value = "";
+    //clear input fields
+    formEl.url.value = '';
+    formEl.alias.value = '';
 }
 
 function sendRequest(url, alias) {
-  var http = new XMLHttpRequest(); //init request object
-  var data = JSON.stringify({ url, alias }); //stringify json with user url and alias
-  http.open("POST", "/", true); //specify type of request and route
-  http.setRequestHeader("Content-Type", "application/json"); //specify MIME type header
+    var http = new XMLHttpRequest(); //init request object
+    var data = JSON.stringify({ url, alias }); //stringify json with user url and alias
+    http.open('POST', '/', true); //specify type of request and route
+    http.setRequestHeader('Content-Type', 'application/json'); //specify MIME type header
 
-  http.onreadystatechange = () => {
-    //function run on response
-    if (http.readyState === 4 && http.status === 200) {
-      //check if response is OK
-      console.log(http.responseText);
-      return http.responseText; //return short url
-    }
-  };
-  http.send(data); //send payload
+    http.onreadystatechange = () => {
+        //function run on response
+        let response = {};
+        if (http.readyState === 4 && http.status === 200) {
+            //check if response is OK
+            console.log(http.responseText);
+            response.url = http.responseText; //return short url
+        } else {
+            parsedResponse = JSON.parse(http.responseText);
+            response.errors = parsedResponse['errors'];
+            // return http.responseText.errors;
+        }
+        console.log('inner response', response);
+        return response;
+    };
+    http.send(data); //send payload
 }
 
 function sendRequestJson(url, alias) {}
--- server.js
+++ server.js
@@ -1,37 +1,48 @@
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const { body, validationResult } = require('express-validator');
 const PORT = 5000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); //set public folder to send our static website
+app.use(cors()); // revent same source complains
+app.use(express.json()); // parse body and to receive the json data
 
-let map = new Map();
+let map = new Map(); //this will be our placeholder database
 
-app.get("/", (req, res) => {
-  console.log(`${req.method} | ${req.url} | ${req.headers["user-agent"]}`);
-  res.send(path.join);
+// @route GET /
+// @desc root route
+app.get('/', (req, res) => {
+    console.log(`${req.method} | ${req.url} | ${req.headers['user-agent']}`);
 });
 
-app.get("/:alias", (req, res) => {
-  const { alias } = req.params;
-  const url = map.get(alias);
-  if (!url) return res.status(400).redirect("/");
-  res.status(200).redirect(url);
+// @route GET /:alias
+// @param alias: name for the shortened url
+app.get('/:alias', (req, res) => {
+    const { alias } = req.params; //grab parameter alias from url
+
+    const url = map.get(alias); //get matching url to the alias from db
+    if (!url) return res.status(400).redirect('/'); //if alias doesn't exist, reload
+    res.status(200).redirect(url); // redirect to long url
 });
 
-app.post("/", (req, res) => {
-  let { url, alias } = req.body;
-  if (!url) return res.status(400).json({ err: "no url" });
-  if (!alias) {
-    alias = Math.random().toString(36).substring(2, 15);
-  }
-  map.set(alias, url);
-  res.status(200).json(`${req.headers.origin}/${alias}`);
+// @route POST /
+// @desc register new url and alias
+app.post('/', [body('url').isURL()], (req, res) => {
+    const errors = validationResult(req); //validate user input
+    if (!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() }); //return errors if present
+
+    let { url, alias } = req.body; //grab user input from body
+    if (!url) return res.status(400).json({ err: 'no url' }); //return error if no url
+    if (!alias) {
+        alias = Math.random().toString(36).substring(2, 15); //generate "unique" alias if not specified
+    }
+    map.set(alias, url);
+    res.status(200).json(`${req.headers.origin}/${alias}`);
 });
 
 app.listen(PORT || 5000, () => {
-  console.log("server listening on port " + PORT);
+    console.log('server listening on port ' + PORT);
 });

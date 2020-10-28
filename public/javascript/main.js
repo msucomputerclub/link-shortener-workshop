b2e6eef51da66de0e49330c3605ee5b252280439
Cristian Novac <crysist14@gmail.com>
2020-10-28 04:27:03 -0400

    switched to axios for less headache

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -13,42 +13,22 @@ function formSubmit(e) {
     console.log(url, alias);
 
     //send request to backend and receive the new url
-    const response = sendRequest(url, alias);
-    console.log('response', response);
-    if (response && response.error) {
-        shortUrlEl.innerHTML = response.errors;
-    }
-    shortUrlEl.innerHTML = `<a href=${response.url}</a>`;
+    axios
+        .post('/', {
+            url,
+            alias,
+        })
+        .then((response) => {
+            console.log('response', response.data.url);
+            shortUrlEl.innerHTML = `<a href="${response.data.url}">${response.data.url}</a>`;
 
-    //insert url into span in the document
-
-    //clear input fields
-    formEl.url.value = '';
-    formEl.alias.value = '';
+            //clear input fields
+            formEl.url.value = '';
+            formEl.alias.value = '';
+        })
+        .catch((error) => {
+            if (error.response) {
+                console.log(error.response.data); // => the response payload
+            }
+        });
 }
-
-function sendRequest(url, alias) {
-    var http = new XMLHttpRequest(); //init request object
-    var data = JSON.stringify({ url, alias }); //stringify json with user url and alias
-    http.open('POST', '/', true); //specify type of request and route
-    http.setRequestHeader('Content-Type', 'application/json'); //specify MIME type header
-
-    http.onreadystatechange = () => {
-        //function run on response
-        let response = {};
-        if (http.readyState === 4 && http.status === 200) {
-            //check if response is OK
-            console.log(http.responseText);
-            response.url = http.responseText; //return short url
-        } else {
-            parsedResponse = JSON.parse(http.responseText);
-            response.errors = parsedResponse['errors'];
-            // return http.responseText.errors;
-        }
-        console.log('inner response', response);
-        return response;
-    };
-    http.send(data); //send payload
-}
-
-function sendRequestJson(url, alias) {}
